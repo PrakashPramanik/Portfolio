8322271a028e79a5f1b92ebaa6bc174dc9aa18f9
Prakash Pramanik <prakashpramanik280@gmail.com>
2024-04-08 14:49:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,181 @@
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
+
+
+
+
+
+
+
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var interval;
+
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
+        interval = setInterval(function () {
+            scrollVertically(targetSection);
+        }, 20);
+    });
+}
+
+function scrollVertically(targetSection) {
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0 || targetSectionCoordinates.bottom <= window.innerHeight) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("scroll",reveal);
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add("active");
+        }
+        else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
